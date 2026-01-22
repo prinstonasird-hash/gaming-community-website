521f4625ed92f229297711a420d683662ac4e335
prinstonasird-hash <prinstonasird@gmail.com>
2026-01-22 17:43:10 +0530

    add chat in forum

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const nav = document.getElementById("nav-menu");
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+function sendMessage() {
+  const input = document.getElementById("chatInput");
+  const text = input.value.trim();
+  if (!text) return;
+
+  const msg = document.createElement("div");
+  msg.className = "message self";
+  msg.textContent = text;
+
+  document.getElementById("chatMessages").appendChild(msg);
+  input.value = "";
+}
